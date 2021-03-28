27d1b94ed05e97e329bcffcb48ea7a351eb4e26e
phanirithvij <phanirithvij2000@gmail.com>
2021-03-28 17:47:05 +0530

    use ordereddict player stunned fix np.all to np.equal print iteration summary player move, perform random action all other actions

--- part_2.py
+++ part_2.py
@@ -1,5 +1,6 @@
 import os
-from typing import Iterable, Tuple
+from collections import OrderedDict
+from typing import Iterable
 import random
 import numpy as np
 
@@ -19,7 +20,7 @@ def prob(p: float):
     return random.random() < p
 
 
-class DotDict(dict):
+class DotDict(OrderedDict):
     """dot.notation access to dictionary attributes"""
     # https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary#comment77092107_23689767
     def __getattr__(*args):
@@ -35,7 +36,21 @@ class DotDict(dict):
         return l
 
 
-class Move:
+class npdict():
+    """To be able to access np array with strings"""
+
+    def __init__(self, nparr, one_one_mapper):
+        self.arr = nparr
+        self.mapper = one_one_mapper
+
+    def __getitem__(self, key):
+        return self.arr[self.mapper[key]]
+
+    def __setitem__(self, key, val):
+        self.arr[self.mapper[key]] = val
+
+
+class PlayerMove:
     LEFT = np.array((-1, 0))
     RIGHT = np.array((1, 0))
     UP = np.array((0, 1))
@@ -53,11 +68,11 @@ class PlayerState:
 
 # unit vectors for directions
 pl_moves = DotDict({
-    "LEFT": tuple(Move.LEFT),
-    "RIGHT": tuple(Move.RIGHT),
-    "UP": tuple(Move.UP),
-    "DOWN": tuple(Move.DOWN),
-    "STAY": tuple(Move.STAY)
+    "LEFT": tuple(PlayerMove.LEFT),
+    "RIGHT": tuple(PlayerMove.RIGHT),
+    "UP": tuple(PlayerMove.UP),
+    "DOWN": tuple(PlayerMove.DOWN),
+    "STAY": tuple(PlayerMove.STAY)
 })
 pl_states = DotDict({
     "W": tuple(PlayerState.W),
@@ -67,23 +82,33 @@ pl_states = DotDict({
     "C": tuple(PlayerState.C)
 })
 
-_INV_STATE_MAP = {pl_states[k]: k for k in pl_states}
+pl_attacks = DotDict({"SHOOT": 25, "HIT": 50})
 
-pl_actions = DotDict(
-    {"SHOOT": 25, "HIT": 50, "CRAFT": 2, "GATHER": 3, "NONE": 4})
+_INV_STATE_MAP = {pl_states[k]: k for k in pl_states}
+_INV_STATE_MAP_INDEX = {k: i for i, k in enumerate(pl_states)}
+
+pl_actions = DotDict({"CRAFT": 2, "GATHER": 3, "NONE": 4})
+
+_INV_ACTIONS_MAP = {pl_actions[k]: k for k in pl_actions}
+_INV_ACTIONS_MAP.update({pl_moves[k]: k for k in pl_moves})
+_INV_ACTIONS_MAP.update({pl_attacks[k]: k for k in pl_attacks})
 
 
 class Player(object):
     MOVES = pl_moves
     STATES = pl_states
-    ATTACKS = pl_actions
+    ATTACKS = pl_attacks
+    ACTIONS = pl_actions
 
     def __init__(self, name: str, inital_state=PlayerState.N):
         self.name = name
         self.state = inital_state
-        self.arrows = 3
+        self._action = Player.ACTIONS.NONE
+        self.arrows = 0
         self.materials = 0
         self.reward = 0
+        self.stunned = False
+        self._values = np.zeros((5,), dtype=np.float)
 
     def move(self, direction):
         # add direction to state
@@ -95,6 +120,14 @@ class Player(object):
     @property
     def cur_state(self) -> str:
         return _INV_STATE_MAP[tuple(self.state)]
+
+    @property
+    def action(self) -> str:
+        return _INV_ACTIONS_MAP[self._action]
+
+    @property
+    def values(self):
+        return npdict(self._values, _INV_STATE_MAP_INDEX)
 
     def jump_to_east(self):
         self.state = PlayerState.E
@@ -108,12 +141,12 @@ class Player(object):
         # any other move for any state is determined i.e. prob = 1
         self.move(direction)
 
-    def defend(self, enemy):
+    def get_wrecked(self):
         if tuple(self.state) not in [Player.STATES.C, Player.STATES.E]:
             return
         self.arrows = 0
         self.reward -= 40
-        enemy.heal()
+        self.stunned = True
 
     def attack(self, enemy, action: int):
         success_prob = 0
@@ -122,16 +155,16 @@ class Player(object):
             if self.arrows == 0:
                 return
             self.arrows -= 1
-            if np.all(self.state, PlayerState.C):
+            if np.array_equal(self.state, PlayerState.C):
                 success_prob = 0.5
-            elif np.all(self.state, PlayerState.W):
+            elif np.array_equal(self.state, PlayerState.W):
                 success_prob = 0.25
-            elif np.all(self.state, PlayerState.E):
+            elif np.array_equal(self.state, PlayerState.E):
                 success_prob = 0.9
         elif action == Player.ATTACKS.HIT:
-            if np.all(self.state, PlayerState.C):
+            if np.array_equal(self.state, PlayerState.C):
                 success_prob = 0.1
-            elif np.all(self.state, PlayerState.E):
+            elif np.array_equal(self.state, PlayerState.E):
                 success_prob = 0.2
 
         # deal appropriate damage with success_probability
@@ -139,21 +172,43 @@ class Player(object):
             enemy.bleed(action)
 
     def craft(self):
-        if self.state != Player.STATES.N or self.materials == 0:
+        if not np.array_equal(self.state, Player.STATES.N) or self.materials == 0:
             return
         self.materials -= 1
         self.arrows += np.random.choice([1, 2, 3], p=[0.5, 0.35, 0.15])
 
     def gather(self):
-        if self.state != Player.STATES.S:
+        if not np.array_equal(self.state, Player.STATES.S):
             return
         if prob(0.75):
             if self.materials == 0:
                 self.materials += 1
 
-    def perform_action(self, action):
-        if action in Player.ATTACKS:
-            print(action)
+    def make_move(self, enemy):
+        if self.stunned:
+            # can't make any move for one round
+            self.stunned = False
+            return
+        choices = []
+        choices.extend(list(Player.ATTACKS.values()))
+        choices.extend(list(Player.MOVES.values()))
+        choices.extend(list(Player.ACTIONS.values()))
+        self._action = random.choice(choices)
+        self.perform_action(enemy)
+
+    def perform_action(self, enemy):
+        action = self._action
+        if action in Player.ATTACKS.values():
+            self.attack(enemy, action)
+        elif action in Player.MOVES.values():
+            self.try_move(random.choice(list(Player.MOVES.values())))
+        elif action in Player.ACTIONS.values():
+            if action == Player.ACTIONS.CRAFT:
+                self.craft()
+            elif action == Player.ACTIONS.GATHER:
+                self.gather()
+            elif action == Player.ACTIONS.NONE:
+                pass
 
     def __str__(self):
         return self.name
@@ -180,22 +235,24 @@ class Enemy(object):
     def get_ready(self):
         self.state = Enemy.STATES.R
 
-    def idle(self):
+    def rest(self):
         self.state = Enemy.STATES.D
 
     def think(self):
         if prob(0.2):
             self.get_ready()
 
-    def try_attack(self):
-        if self.state == Enemy.STATES.R:
-            if prob(0.5):
-                # todo attack
-                self.idle()
+    def try_attack(self, player: Player):
+        if prob(0.5):
+            # attack
+            player.get_wrecked()
+            self.heal()
+            self.rest()
 
-    def do_your_thing(self):
+    def make_move(self, player: Player):
         self.think()
-        self.try_attack()
+        if self.state == Enemy.STATES.R:
+            self.try_attack(player)
 
     def __str__(self):
         return self.name
@@ -211,9 +268,17 @@ def loop():
     while True:
         iter_count += 1
         print("iteration=", iter_count, sep='')
-        ij.try_move(random.choice(list(Player.MOVES.values())))
-        print(ij.state)
-        print(ij.cur_state)
+        mm.make_move(ij)
+        ij.make_move(mm)
+        # print(ij.cur_state)
+        # print(ij.materials)
+        # print(ij.arrows)
+        # print(mm.state)
+        # print(mm.health)
+        # print(ij.action)
+        # print(ij.values[ij.cur_state])
+        print(
+            f"({ij.cur_state},{ij.materials},{ij.arrows},{mm.state},{mm.health}):{ij.action}=[{ij.values[ij.cur_state]}]")
         if iter_count >= max_tries or mm.dead:
             print("Game Over")
             return
